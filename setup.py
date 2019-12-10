a6e00bb773d1e5277413118e46b5c9ff06832f1a
Markus Holtermann <info@markusholtermann.eu>
2019-12-10 10:40:19 +0100

    Setup project

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from pkg_resources.extern.packaging.version import Version
+from setuptools import find_packages, setup
+
+
+setup(
+    name="code_sound",
+    author="Markus Holtermann, Michael Kleen, Sergei Zyubin",
+    author_email="info+code-sound@markusholtermann.eu",
+    url="https://github.com/MarkusH/code-sound",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
