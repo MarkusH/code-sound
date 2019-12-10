e6c30489c66a9c062776460cf13471cc19af9229
Michael Kleen <michael.kleen@crate.io>
2019-12-10 19:17:56 +0100

    Reformat code

--- code_sound/snd/models.py
+++ code_sound/snd/models.py
@@ -33,58 +33,57 @@ def types_to_sound(code_types: Iterable[ast.stmt]) -> Generator[Sound, None, Non
         print(code_type)
         if isinstance(code_type, ast.Module):
             yield Sound(Effect("synth", duration, sample, "D3"),
-			Effect("fade", 0.01, 0, 0.1))
+                        Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.stmt):
             yield Sound(Effect("synth", duration, sample, "Bb3"))
         elif isinstance(code_type, ast.Expression):
             yield Sound(
                 Effect("synth", duration, sample, "C3"),
-			Effect("fade", 0.01, 0, 0.1))
+                Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.Assign):
             yield Sound(
                 Effect("synth", duration, sample, "Db3"),
-			Effect("fade", 0.01, 0, 0.1))
+                Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.If):
             yield Sound(
                 Effect("synth", duration, sample, "Eb3"),
-			Effect("fade", 0.01, 0, 0.1))
+                Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.While):
             yield Sound(
                 Effect("synth", duration, sample, "E3"),
-			Effect("fade", 0.01, 0, 0.1))
+                Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.For):
             yield Sound(
                 Effect("synth", duration, sample, "F3"),
-			Effect("fade", 0.01, 0, 0.1))
+                Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.Name):
             yield Sound(
                 Effect("synth", duration, sample, "Gb3"),
-			Effect("fade", 0.01, 0, 0.1))
+                Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.Call):
             yield Sound(
                 Effect("synth", duration, sample, "Ab3"),
-			Effect("fade", 0.01, 0, 0.1))
+                Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.Num):
             yield Sound(
                 Effect("synth", duration, sample, "A3"),
-			Effect("fade", 0.01, 0, 0.1))
+                Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.Attribute):
             yield Sound(
                 Effect("synth", duration, sample, "B3"),
-		Effect("fade", 0.01, 0, 0.1)
+                Effect("fade", 0.01, 0, 0.1)
             )
         elif isinstance(code_type, ast.FunctionDef):
             yield Sound(Effect("synth", duration, "pl", "Bb3"),
-			Effect("fade", 0.01, 0, 0.1))
+                        Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.Import):
             yield Sound(Effect("synth", duration, "pl", "G2"),
-			Effect("fade", 0.01, 0, 0.1))
+                        Effect("fade", 0.01, 0, 0.1))
         elif isinstance(code_type, ast.Expr):
             yield Sound(
                 Effect("synth", duration, "pl", "F2"),
-		Effect("fade", 0.01, 0, 0.1)
+                Effect("fade", 0.01, 0, 0.1)
             )
         elif isinstance(code_type, ast.stmt):
             yield Sound(Effect("synth", duration, "pl", "E2"),
-			Effect("fade", 0.01, 0, 0.1))
-
+                        Effect("fade", 0.01, 0, 0.1))
