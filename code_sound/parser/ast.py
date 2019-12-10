934c4b1f7604eec19350e8288468aa3923285eb1
Michael Kleen <michael.kleen@crate.io>
2019-12-10 17:47:40 +0100

    Remove types and work directly on ast types

--- code_sound/parser/ast.py
+++ code_sound/parser/ast.py
@@ -1,38 +1,13 @@
 import ast
 from typing import Generator
 
-from .types import BaseType, Expression, Module, Statement, Assign, For, If, While, Call, Name, Num, Attribute, Tuple
 
-
-def parse(content: str) -> Generator[BaseType, None, None]:
+def parse(content: str) -> Generator[ast.stmt, None, None]:
     tree = ast.parse(content)
     current_type = None
     next_type = None
     for node in ast.walk(tree):
-        if isinstance(node, ast.Module):
-            next_type = Module()
-        elif isinstance(node, ast.Assign):
-            next_type = Assign()
-        elif isinstance(node, ast.For):
-            next_type = For()
-        elif isinstance(node, ast.If):
-            next_type = If()
-        elif isinstance(node, ast.While):
-            next_type = While()
-        elif isinstance(node, ast.Call):
-            next_type = Call()
-        elif isinstance(node, ast.Name):
-            next_type = Name()
-        elif isinstance(node, ast.Num):
-            next_type = Num()
-        elif isinstance(node, ast.Attribute):
-            next_type = Attribute()
-        elif isinstance(node, ast.Tuple):
-            next_type = Tuple()
-        elif isinstance(node, ast.expr):
-            next_type = Expression()
-        elif isinstance(node, ast.stmt):
-            next_type = Statement()
+        next_type = node
 
         if current_type is None:
             current_type = next_type
@@ -40,4 +15,3 @@ def parse(content: str) -> Generator[BaseType, None, None]:
         if current_type.__class__ != next_type.__class__:
             yield current_type
             current_type = next_type
-
--- code_sound/snd/models.py
+++ code_sound/snd/models.py
@@ -1,6 +1,6 @@
+import ast
 from typing import Any, Dict, Generator, Iterable, Optional, Tuple
 
-from ..parser.types import BaseType, Expression, Module, Statement, Assign, If, While, For, Name, Call, Num, Attribute
 
 SND_FX_ARGS_TYPE = Tuple[Optional[Tuple[Any]], Optional[Dict[str, Any]]]
 
@@ -26,56 +26,56 @@ class Sound:
         return fx
 
 
-def types_to_sound(code_types: Iterable[BaseType]) -> Generator[Sound, None, None]:
+def types_to_sound(code_types: Iterable[ast.stmt]) -> Generator[Sound, None, None]:
     duration = 0.2
     for code_type in code_types:
         print(code_type)
-        if isinstance(code_type, Module):
+        if isinstance(code_type, ast.Module):
             yield Sound(Effect("synth", duration, "pl", "D2"), Effect("reverb"))
-        elif isinstance(code_type, Statement):
-            yield Sound(Effect("synth", duration, "pl", "E2"))
-        elif isinstance(code_type, Expression):
-            yield Sound(
-                Effect("synth", duration, "pl", "F2"),
-                Effect("reverb")
-            )
-        elif isinstance(code_type, Assign):
+        elif isinstance(code_type, ast.Assign):
             yield Sound(
                 Effect("synth", duration, "pl", "G2"),
                 Effect("reverb")
             )
-        elif isinstance(code_type, If):
+        elif isinstance(code_type, ast.If):
             yield Sound(
                 Effect("synth", duration, "pl", "A2"),
                 Effect("reverb")
             )
-        elif isinstance(code_type, While):
+        elif isinstance(code_type, ast.While):
             yield Sound(
                 Effect("synth", duration, "pl", "Bb2"),
                 Effect("reverb")
             )
-        elif isinstance(code_type, For):
+        elif isinstance(code_type, ast.For):
             yield Sound(
                 Effect("synth", duration, "pl", "C2"),
                 Effect("reverb")
             )
-        elif isinstance(code_type, Name):
+        elif isinstance(code_type, ast.Name):
             yield Sound(
                 Effect("synth", duration, "pl", "Bb2"),
                 Effect("reverb")
             )
-        elif isinstance(code_type, Call):
+        elif isinstance(code_type, ast.Call):
             yield Sound(
                 Effect("synth", duration, "pl", "A2"),
                 Effect("reverb")
             )
-        elif isinstance(code_type, Num):
+        elif isinstance(code_type, ast.Num):
             yield Sound(
                 Effect("synth", duration, "pl", "C1"),
                 Effect("reverb")
             )
-        elif isinstance(code_type, Attribute):
+        elif isinstance(code_type, ast.Attribute):
             yield Sound(
                 Effect("synth", duration, "pl", "G2"),
+                Effect("reverb")
+            )
+        elif isinstance(code_type, ast.stmt):
+            yield Sound(Effect("synth", duration, "pl", "E2"))
+        elif isinstance(code_type, ast.Expression):
+            yield Sound(
+                Effect("synth", duration, "pl", "F2"),
                 Effect("reverb")
             )
